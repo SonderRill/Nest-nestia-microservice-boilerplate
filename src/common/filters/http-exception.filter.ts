c480f051e42fc8d8bcb4698399c738755c817232
Berezkin Kirill <killer.kir@inbox.ru>
2025-11-06 02:42:52 +0600

    Update .env.example for NODE_ENV and enhance HttpExceptionFilter with logging and error handling improvements

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -1,22 +1,35 @@
-import { ArgumentsHost, Catch, HttpException } from '@nestjs/common'
+import { ArgumentsHost, Catch, HttpException, Logger } from '@nestjs/common'
 import { BaseExceptionFilter } from '@nestjs/core'
+import { FastifyRequest } from 'fastify'
+import { TypeGuardError } from 'typia'
 
-interface CommonError {
-  error?: string
-  errors?: [
-    {
-      path: string
-      expected: string
-      value: any
-    },
-  ]
-  message: string
+import { sanitizeHeaders } from '../utils'
+
+export interface TypiaBadRequestException
+  extends Omit<TypeGuardError, 'method'> {
   statusCode: number
+}
+
+export interface CommonError {
+  error?: string
+  message?: string
+  statusCode: number
+  stack?: string
+  code?: string
 }
 
 @Catch()
 export class HttpExceptionFilter extends BaseExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name)
+
+  constructor() {
+    super()
+  }
+
   catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const request = ctx.getRequest<FastifyRequest>()
+
     if (exception instanceof HttpException) {
       const status = exception.getStatus()
       const exceptionResponse = exception.getResponse() as CommonError | string
@@ -24,13 +37,26 @@ export class HttpExceptionFilter extends BaseExceptionFilter {
       if (typeof exceptionResponse === 'object' && exceptionResponse !== null) {
         if (
           exceptionResponse.message !== undefined &&
-          typeof exceptionResponse.message !== 'string'
+          typeof exceptionResponse.message !== 'string' &&
+          typeof exceptionResponse.message !== 'number'
         ) {
           throw new Error('message should be a string')
         }
 
+        this.logger.error({
+          exception,
+          headers: sanitizeHeaders(request.headers),
+          url: request.url,
+        })
+
+        if (process.env.NODE_ENV === 'development') {
+          exceptionResponse.stack = exception.stack
+        }
+
         exceptionResponse.statusCode = status
-        exceptionResponse.message ||= exception.message
+        exceptionResponse.message = (
+          exceptionResponse.message || exception.message
+        ).toString()
       }
     }
 
